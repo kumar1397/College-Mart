28b44ecb241bc22a57815644eda68eebbe64ee43
Anubhav shaurya <97554087+anubhav220@users.noreply.github.com>
2024-06-16 14:00:48 +0530

    updaed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Auth from './components/Auth'
 import Profile from './components/profile/profile';
+
+import ProductFormCard from './components/product/ProductCard';
 function App() {
   return(
     <>
- <Profile/> 
+ {/* <Profile/>  */}
  {/* <Auth/> */}
+< ProductFormCard />
     </>
   )
 }
