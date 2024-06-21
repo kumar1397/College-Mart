7841c4a19b5fe8e2c8a298b12505912030397d24
Mahaprasad Prusty <mahaprasadprusty72@gmail.com>
2024-06-22 03:51:11 +0530

    updated card component

--- src/components/home-page/cardcomponent.js
+++ src/components/home-page/cardcomponent.js
@@ -5,7 +5,7 @@ export default function cardcomponent(props) {
   return (
     <div className='complete-card'>
     <div className='cards'>
-      <h1 className=''>MP</h1>
+      {/* <h1 className=''></h1> */}
     </div>
     <div>{props.name}</div>
     <div>{props.price}</div>
