2c36049a7e5ca1f3d23dd49b255e4f739a246a8f
Mahaprasad Prusty <mahaprasadprusty72@gmail.com>
2024-06-26 23:39:30 +0530

    error

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import Auth from './components/Auth'
-import Home from './components/home-page/home'
-import Hero from './components/Hero'
-import { Route, Routes,BrowserRouter as Router } from 'react-router-dom';
-import ProductFormCard from './components/product/ProductCard';
+// import Auth from './components/Auth'
+// import Profile from './components/profile/profile';
+// import Home from './components/home-page/home'
+
+// import ProductFormCard from './components/product/ProductCard';
 function App() {
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Hero />} />
-          <Route path="/auth" element={<Auth/>} />
-          <Route path="/home" element={<Home/>} />
-        </Routes>
-      </Router>
-
-    </div>
-
+  return(
+    <>
+ {/* <Profile/>  */}
+ {/* <Auth/> */}
+{/* < ProductFormCard /> */}
+    {/* <Home/> */}
+    <h1>Kumaran</h1>
+    </>
   )
 }
 
