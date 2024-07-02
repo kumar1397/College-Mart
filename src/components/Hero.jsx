15d8b2882b18de2243d22935e9d957be1aa072bc
kumar1397 <sahuiswar007@outlook.com>
2024-07-02 17:25:24 +0530

    1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,20 @@ import Heronavbar from './Heronavbar'
 import { FaInstagram } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
-
+import { Link } from 'react-router-dom';
 const Hero = () => {
   return (
     <div>
       <Heronavbar />
-      <div className='herohomebg w-stretch h-[50vh]'>
-        content
+      <div className='herohomebg w-stretch h-[50vh] flex justify-center text-white'>
+        <div className="flex flex-col justify-center">
+          <div className="font-semibold flex items-center uppercase">
+            <div className="w-10 h-[2px] mr-3 bg-cyan-700"></div>The
+          </div>
+          <h1 className="uppercase text-[55px] md:text-[70px] leading-[1.1] font-semibold mb-4">College Mart <br />
+            <span className="font-light">one stop solution</span></h1>
+          <Link to={'/'} className='self-start uppercase font-semibold border-b-2 border-primary'>Discover More</Link>
+        </div>
       </div>
       <div className='flex flex-col items-center bg-'>
         <div></div>
