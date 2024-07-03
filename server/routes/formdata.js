f96f3e9783598d063e44e87a9d326327faf1fc46
kumar1397 <sahuiswar007@outlook.com>
2024-07-03 11:27:43 +0530

    hero page

--- server/routes/formdata.js
+++ server/routes/formdata.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 const {localFileUpload,fileUpload} = require('../controllers/Form') 
-router.post('/localfileupload',localFileUpload);
+
 router.post('/fileupload',fileUpload);
 
 module.exports = router
--- src/components/Heronavbar.jsx
+++ src/components/Heronavbar.jsx
@@ -3,12 +3,24 @@ import React from 'react'
 const Heronavbar = () => {
   return (
     <div>
-        <div className='w-full h-16 text-lg font-medium flex flex-row justify-end items-center gap-10 pr-10 hnavbg text-[#f5f5f5]'>
-            <div>Home</div>
-            <div>About us</div>
-            <div>Contact</div>
-            <div>Sign In</div>
+      <div className='w-full h-16 text-lg font-semibold flex flex-row justify-end items-center gap-10 pr-10 hnavbg text-[#f5f5f5]'>
+        <div>
+          <span>Home</span>
+          <div className={`w-14 h-1 bg-[#f5f5f5] hidden group-hover:block `}></div>
         </div>
+        <div>
+          <span>About us</span>
+          <div className={`w-18 h-1 bg-[#f5f5f5] hidden`}></div>
+        </div>
+        <div>
+          <span>Contact</span>
+          <div className={`w-18 h-1 bg-[#f5f5f5] hidden`}></div>
+        </div>
+        <div>
+          <span>Sign In</span>
+          <div className={`w-18 h-1 bg-[#f5f5f5] hidden`}></div>
+        </div>
+      </div>
     </div>
   )
 }
