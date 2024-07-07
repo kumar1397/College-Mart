2002481149e30c2d756e5734e72ee07b7c2ff147
prateek-10 <prateeksibu333@gmail.com>
2024-07-07 23:25:31 +0530

    route-update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
 import Auth from "./components/Auth";
 import Hero from "./components/hero/Hero";
 import Home02 from "./components/new-home-page/parent";
 import { AuthProvider } from "./contexts/AuthContext";
-
+import ContactUs from "./components/contact-us/contact";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import ProductFormCard from "./components/product/newProduct";
 import Profile from "./components/profile/profile";
@@ -19,6 +18,7 @@ function App() {
             <Route path="/home" element={<Home02 />} />
             <Route path="/home/profile" element={<Profile />} />
             <Route path="/home/form" element={<ProductFormCard />} />
+            <Route path="/contactus" element={<ContactUs />} />
           </Routes>
         </AuthProvider>
       </Router>
