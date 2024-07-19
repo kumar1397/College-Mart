832b03ce32f3bb3474cdc6f59331747f95cae97e
prateek-10 <prateeksibu333@gmail.com>
2024-07-20 02:33:54 +0530

    test change

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -6,7 +6,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 const singleUploadMiddleware = upload.single("avatar");
-const multipleUploadMiddleware = upload.array("images", 10); // Limit to 10 files
+const multipleUploadMiddleware = upload.single("images"); // Limit to 10 files
 
 module.exports = {
   singleUploadMiddleware,
