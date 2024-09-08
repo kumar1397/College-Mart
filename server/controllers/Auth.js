60f673caaf11e560e3040a65b4e876ccdcc7f8c6
kumar1397 <sahuiswar007@outlook.com>
2024-09-08 23:28:52 +0530

    commment otp

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -18,49 +18,49 @@ app.use(
   })
 );
 
-exports.sendotp = async (req, res) => {
-  try {
-    const { email } = req.body
-    const checkUserPresent = await User.findOne({ email })
+// exports.sendotp = async (req, res) => {
+//   try {
+//     const { email } = req.body
+//     const checkUserPresent = await User.findOne({ email })
 
-    if (checkUserPresent) {
-      return res.status(401).json({
-        success: false,
-        message: `User is Already Registered`,
-      })
-    }
+//     if (checkUserPresent) {
+//       return res.status(401).json({
+//         success: false,
+//         message: `User is Already Registered`,
+//       })
+//     }
 
-    var otp = otpGenerator.generate(6, {
-      upperCaseAlphabets: false,
-      lowerCaseAlphabets: false,
-      specialChars: false,
-    })
-    const result = await OTP.findOne({ otp: otp })
-    console.log("Result is Generate OTP Func")
-    console.log("OTP", otp)
-    console.log("Result", result)
-    while (result) {
-      otp = otpGenerator.generate(6, {
-        upperCaseAlphabets: false,
-      })
-    }
-    const otpPayload = { email, otp }
-    const otpBody = await OTP.create(otpPayload)
-    console.log("OTP Body", otpBody)
-    res.status(200).json({
-      success: true,
-      message: `OTP Sent Successfully`,
-      otp,
-    })
-  } catch (error) {
-    console.log(error.message)
-    return res.status(500).json({ success: false, error: error.message })
-  }
-}
+//     var otp = otpGenerator.generate(6, {
+//       upperCaseAlphabets: false,
+//       lowerCaseAlphabets: false,
+//       specialChars: false,
+//     })
+//     const result = await OTP.findOne({ otp: otp })
+//     console.log("Result is Generate OTP Func")
+//     console.log("OTP", otp)
+//     console.log("Result", result)
+//     while (result) {
+//       otp = otpGenerator.generate(6, {
+//         upperCaseAlphabets: false,
+//       })
+//     }
+//     const otpPayload = { email, otp }
+//     const otpBody = await OTP.create(otpPayload)
+//     console.log("OTP Body", otpBody)
+//     res.status(200).json({
+//       success: true,
+//       message: `OTP Sent Successfully`,
+//       otp,
+//     })
+//   } catch (error) {
+//     console.log(error.message)
+//     return res.status(500).json({ success: false, error: error.message })
+//   }
+// }
 
 exports.signup = async (req, res) => {
   try {
-    const { name, phone, email, password, cpassword, otp } = req.body;
+    const { name, phone, email, password, cpassword } = req.body;
 
     // Check if user with the given email already exists
     const existingUser = await User.findOne({ email });
@@ -71,21 +71,21 @@ exports.signup = async (req, res) => {
         message: "User already exists",
       });
     }
-    const response = await OTP.find({ email }).sort({ createdAt: -1 }).limit(1)
-    console.log(response)
-    if (response.length === 0) {
-      // OTP not found for the email
-      return res.status(400).json({
-        success: false,
-        message: "The OTP is not valid",
-      })
-    } else if (otp !== response[0].otp) {
-      // Invalid OTP
-      return res.status(400).json({
-        success: false,
-        message: "The OTP is not valid",
-      })
-    }
+    // const response = await OTP.find({ email }).sort({ createdAt: -1 }).limit(1)
+    // console.log(response)
+    // if (response.length === 0) {
+    //   // OTP not found for the email
+    //   return res.status(400).json({
+    //     success: false,
+    //     message: "The OTP is not valid",
+    //   })
+    // } else if (otp !== response[0].otp) {
+    //   // Invalid OTP
+    //   return res.status(400).json({
+    //     success: false,
+    //     message: "The OTP is not valid",
+    //   })
+    // }
 
     // Check if password and confirm password match
     if (password !== cpassword) {
